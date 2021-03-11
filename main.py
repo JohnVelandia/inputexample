349dcc383fafb46ea5472bf2d396f02f10a95df2
jvelandia <javelandia@ucatolica.edu.co>
2021-03-10 23:36:27 -0500

    My fist input

---
+++ main.py
@@ -0,0 +1,12 @@
+name = input("What´s your name")
+print("The name is", name)
+
+greeting= "How old are you "+name+"?"
+age= input(greeting)
+year=2021-int(age)
+print(name, " you were born in ", year)
+
+
+
+
+
